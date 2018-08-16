1b8b137fd0ebc62b64bdd685af823667c73942ba
Min Yang (InConsulting Inc.) <v-miyan@microsoft.com>
2018-08-16 16:59:29 -0700

    finished ConsoleBoardRenderer: - gray background, red foreground, black/white pieces - pieces by label

--- Chess/Chess/ConsoleBoardRenderer.cs
+++ Chess/Chess/ConsoleBoardRenderer.cs
@@ -18,16 +18,56 @@ namespace Chess
         /// <param name="board">The board</param>
         public void Render(IBoard board)
         {
+            string newLine = "\n________________________________________________________\n                                                        ";
             // validate the arguments
             // if board is null, throw ArgumentNullException
+            if (board == null)
+            {
+                throw new ArgumentNullException();
+            }
 
             // draw the board to the console, however you see fit
             // each piece has a ToString() you can use to draw it
 
+            Console.BackgroundColor = ConsoleColor.Gray;
+
             // Use colors to make your output look prettier
             // Each piece has an owner so you can use that to drive the color
             // If you don't know how to do colors in the Console, look it up :)
-            throw new NotImplementedException();
+            for (int i = 0; i < board.NumRows; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine(newLine);
+                for (int j = 0; j < board.NumColumns; j++)
+                {
+                    var position = new Position(i, j);
+                    var currentPiece = board.GetPiece(position);
+                    if (currentPiece == null)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.Write("|     |");
+                    }
+                    else if (currentPiece.Owner.Color == PlayerColor.White)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.Write("|");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write($"  {currentPiece.Label}  ");
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.Write("|");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.Write("|");
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.Write($"  {currentPiece.Label}  ");
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.Write("|");
+                    }
+                }
+            }
+            Console.WriteLine(newLine);
         }
     }
 }
