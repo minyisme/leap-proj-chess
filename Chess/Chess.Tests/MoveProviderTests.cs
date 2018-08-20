1b78f9f0a5d09b3a16a50bea6b40bf6c95112ba1
Min Yang (InConsulting Inc.) <v-miyan@microsoft.com>
2018-08-20 16:31:39 -0700

    created test for GetMove - mocked inputprovider

---
+++ Chess/Chess.Tests/MoveProviderTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Chess.Interfaces;
+using Moq;
+
+namespace Chess.Tests
+{
+    [TestClass]
+    public class MoveProviderTests
+    {
+        [TestMethod]
+        public void GetMoveTest()
+        {
+            var mockIP = new Mock<IInputProvider>();
+
+            mockIP.Setup(x => x.Read())
+                  .Returns("3");
+
+            var moveProvider = new MoveProvider(mockIP.Object);
+
+            var expected = new Move();
+            expected.Source = new Position(3, 3);
+            expected.Dest = new Position(3, 3);
+
+            var actual = moveProvider.GetMove();
+            Assert.AreEqual(expected.Source.Row, actual.Source.Row);
+            Assert.AreEqual(expected.Source.Column, actual.Source.Column);
+            Assert.AreEqual(expected.Dest.Row, actual.Dest.Row);
+            Assert.AreEqual(expected.Dest.Column, expected.Dest.Column);
+        }
+    }
+}
